2b6d47413b739ce1ba6a92809b049d8f3a1d1b6d
seong9566 <dlgustjd9566@gmail.com>
2023-01-05 00:05:20 +0900

    세균 증식

--- src/Squre/Study.java
+++ src/Squre/Study.java
@@ -1,7 +1,24 @@
 package Squre;
 
+class Solution {
+	public int solution(int n, int t) {
+		// 풀이 1
+		int answer = n * (int) Math.pow(2, t);
+		// 풀이 2
+		int answer2 = n;
+		for (int i = 0; i < t; i++) {
+			answer2 = answer2 + answer2;
+		}
+		System.out.println(n + " 마리 " + t + " 시간 " + "결과 : " + answer);
+		return answer;
+	}
+
+}
+
 public class Study {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Solution s = new Solution();
+		s.solution(2, 10);
+		s.solution(7, 15);
 	}
 }

1af5d92d2dcf4337968730d533a27d4cad035bdf
seong9566 <dlgustjd9566@gmail.com>
2023-01-05 23:03:27 +0900

    피자나눠먹기

--- src/Squre/Study.java
+++ src/Squre/Study.java
@@ -1,7 +1,29 @@
 package Squre;
 
+class Solution {
+	public int solution(int n) {
+		int answer = 1;
+		/**
+		 * 피자는 7조각
+		 * 피자를 나눠먹을 사람의 수 n
+		 * 모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자수 구하기
+		 */
+		int pizza = 7;
+		int mok = n / pizza;
+		int na = n % pizza;
+		answer = mok;
+		if (na != 0) {
+			answer++;
+		}
+
+		System.out.println("answer : " + answer);
+		return answer;
+	}
+}
+
 public class Study {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Solution s = new Solution();
+		s.solution(22);
 	}
 }

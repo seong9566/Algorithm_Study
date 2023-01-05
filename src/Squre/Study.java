eaa1527ec36d459497b134b64f7b136095ac37ad
seong9566 <dlgustjd9566@gmail.com>
2023-01-05 22:10:59 +0900

    삼총사

--- src/Squre/Study.java
+++ src/Squre/Study.java
@@ -1,7 +1,25 @@
 package Squre;
 
+class Solution {
+	public int solution(int[] number) {
+		int answer = 0;
+		for (int i = 0; i < number.length; i++) {
+			for (int j = i + 1; j < number.length; j++) {
+				for (int a = j + 1; a < number.length; a++) {
+					if (number[i] + number[j] + number[a] == 0)
+						answer++;
+				}
+			}
+		}
+		System.out.println("answer = " + answer);
+		return answer;
+	}
+}
+
 public class Study {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Solution s = new Solution();
+		int[] number = { -2, 3, 0, 2, -5 };
+		s.solution(number);
 	}
 }
